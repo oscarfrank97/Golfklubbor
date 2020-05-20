03e0a68ae03d910bed140075f60b034017c1269c
Oscar Frank <oscarfrank97@gmail.com>
2020-05-20 17:47:54 +0200

    Update 1

---
+++ login.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+        <title> Welcome to GolfGear </title>
+        <link rel="stylesheet"type="text.css"href="Oscar.css">
+</head>
+<body>
+<div class="Log">
+    <h2>Login here</h2>
+    <form class="LogRegBox" action="validation.php" method="post">
+        <div class="">
+            <label> Username </label>
+            <input type="text" id="Username" class="" required>
+        </div>
+        <div class="">
+            <label> Password </label>
+            <input type="password" id="Password" class="" required>
+        </div>
+            <button type="sumbit" class=""> Login </button>
+    </form>
+</div>
+<div class="Reg">
+        <h2>Register Here</h2>
+    <form class="LogRegBox"action="registration.php" method="post">
+        <div class="">
+            <label> Username </label>
+            <input type="text" id="Username" class="" required>
+        </div>
+        <div class="">
+            <label> Password </label>
+            <input type="password" id="Password" class="" required>
+        </div>
+            <button type="sumbit" class=""> Register </button>
+
+    </form>
+</div>
