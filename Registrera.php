0e1f4379e6e5db7890a0e7974f220bec81fb9052
Oscar Frank <oscarfrank97@gmail.com>
2020-05-25 14:28:24 +0200

    Löst koppling till databas

--- Registrera.php
+++ Registrera.php
@@ -1,31 +1,4 @@
 <?php
+$db = new SQLite3("GolfGear.db");
 
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "golfgear"
-
-//Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-//check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
-
-//prepare and bind
-$stmt = $conn->prepare("INSERT INTO register(Email, Username, Password)
-VALUES(?, ?, ?)");
-$stmt->bind_param("sss", $Email, $Username, $Password);
-
-// set parameters and execute
-$Email = "blalala@gmail.com";
-$Username = "Martin";
-$Password = "hejejeje";
-$stmt->execute();
-
-echo "New record created successfully";
-$stmt->close();
-$conn->close();
 ?>
