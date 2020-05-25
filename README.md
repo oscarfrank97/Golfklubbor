c42f3fa462cc378fc87a034c67dc25a2883c77a2
Oscar Frank <oscarfrank97@gmail.com>
2020-05-25 12:57:01 +0200

    Fixat PHP och HTML för att ansluta till databas

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Golfklubbor
+
 Inlämmning
--- Script.js
+++ Script.js
@@ -1,53 +1,53 @@
-function ValideraRegg()
+// TODO: Vet ej om man ska använda i regga.html eller Registrers.php
+function test_input($data)
 {
-    var email = document.Registrera.email.value;
-    var username = document.Registrera.username.value;
-    var password = document.Registrera.password.value;
-    var emailkrav1 = email.indexOf('@');
-    var emailkrav2 = email.lastindexOf('.')
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 
 
-    if (email.trim() == "")
-    {
-        alert("Du behöver fylla i en giltig Email adress")
-    }
-    else if (username.trim() == "")
-    {
-        alert("Du behöver välja ett användarnamn")
-    }
-    else if ((emailkrav1 < 0) || emailkrav1 > emailkrav2 )
-    {
-        alert("Email-adressen verkar ha fel format, vänligen försök igen")
 
-    }
-    else if ((password.trim() == "") || (password.lenght < 5))
-    {
-        alert("Lösenordet måste vara minst fem tecken")
-    }
-    else
-    {
-        alert("Välkommen till GolfGear ditt konto är nu skapat!")
-    }
+
+function ValideraRegg()// TODO: Skapa filter för värderna.
+// TODO: Hasha & salta lösenordet.
+{
+  var email = document.Registrera.email.value;
+  var username = document.Registrera.username.value;
+  var password = document.Registrera.password.value;
+  var emailkrav1 = email.indexOf('@');
+  var emailkrav2 = email.lastindexOf('.')
+
+
+  if (email.trim() == "") {
+    alert("Du behöver fylla i en giltig Email adress")
+  } else if (username.trim() == "") {
+    alert("Du behöver välja ett användarnamn")
+  } else if ((emailkrav1 < 0) || emailkrav1 > emailkrav2) {
+    alert("Email-adressen verkar ha fel format, vänligen försök igen")
+
+  } else if ((password.trim() == "") || (password.lenght < 5)) {
+    alert("Lösenordet måste vara minst fem tecken")
+  } else {
+    alert("Välkommen till GolfGear ditt konto är nu skapat!")
+  }
 
 
 }
-function ValideraInlogg()
-{
-    var username = document.Inlogg.username.value;
-    var password = document.Inlogg.password.value;
 
-    if (username.trim() == "")
-    {
-        alert("Fyll i ditt användarnamn för att logga in")
-    }
-    else if(password.trim() == "")
-    {
-        alert("Fyll i ditt lösenord för att logga in")
-    }
-    else
-    {
-        alert("Välkommen hoppas du finner din vistelse givande")
-    }
+function ValideraInlogg() // TODO: Bygg ut denna för att förhindra att användare skriver in fel saker //
+ {
+  var username = document.Inlogg.username.value;
+  var password = document.Inlogg.password.value;
+
+  if (username.trim() == "") {
+    alert("Fyll i ditt användarnamn för att logga in")
+  } else if (password.trim() == "") {
+    alert("Fyll i ditt lösenord för att logga in")
+  } else {
+    alert("Välkommen hoppas du finner din vistelse givande")
+  }
 
 
 }
