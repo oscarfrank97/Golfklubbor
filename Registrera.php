ca15a0524a32c6101b6a91fbb3d39d39f62ffec8
Oscar Frank <oscarfrank97@gmail.com>
2020-05-25 13:11:38 +0200

    Fortsättning PHP to databse

--- Registrera.php
+++ Registrera.php
@@ -1,44 +1,25 @@
 <?php
-//if (isset($_POST['skapa konto'])) {
+if (isset($_POST['skapa konto'])) {
   $Email = $_POST['Email']
   $Username = $_POST['Username']
   $Password = $_POST['Password']
 
-  $conn = new mysqli('localhost', 'root', 'root', 'golfgear')
-  if ($conn->mysqli_connect_error) {
-     die('Connection Failed : '.$conn->mysqli_connect_error);
-  }else {
-    $stmt = $conn->prepare("INSERT INTO register(Email, Username, Password)
-            values(Email, Username, Password)");
-    $stmt->execute();
-    echo "Registrering lyckades";
-    $stmt->close();
-    $conn->close();
-  }
-//}
-//
-/*
-if ((!empty($Email)) && (!empty($Username)) && (!empty($Password)) ) {
-  $host = "localhost";
-  $dbusername = "root";
-  $dbpassword = "root";
-  $dbname = "golfgear";
-// Skapar anslutning till databasen
-$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-if (mysqli_connect_error()){
-die('Connection Error (' . mysqli_connect_errno() .') '. mysqli_connect_error());
+  $user = 'root';
+  $password = 'root';
+  $db = 'register';
+  $host = 'localhost';
+  $port = 8889;
+
+  $link = mysqli_init();
+  $success = mysqli_real_connect(
+     $link,
+     $host,
+     $user,
+     $password,
+     $db,
+     $port
+  );
+
 }
-else{
-  $sql = "INSERT INTO register (Email, Username, Password)
-  values ('$Email', '$Username', '$Password')";
-  if($conn->query($sql)){
-    echo "Grattis du har nu skapat ett konto";
-    else{
-      echo "Error: ". $sql ."<br>". $conn->error;
-    }
-    $conn->close();
-  }
-}
-//
-}
- ?>
+
+?>
